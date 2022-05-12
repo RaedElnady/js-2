fe55ca7f4ac2d81446e4bb7bd3815e5c8757fb53
RaedElnady <raed.elnady4@gmail.com>
2022-05-12 17:51:00 +0300

    Initial Update

---
+++ js/main.js
@@ -0,0 +1,26 @@
+var quotes = [
+{
+    author:"Bernard M. Baruch",
+    quoteDescription:"“Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind.”",
+},
+{
+    author:"Mae West",
+    quoteDescription:"“You only live once, but if you do it right, once is enough.”",
+},
+{
+    author:"Oscar Wilde",
+    quoteDescription:"“I am so clever that sometimes I don't understand a single word of what I am saying.”",
+},
+{
+    author:"Mark Twain",
+    quoteDescription:"“If you tell the truth, you don't have to remember anything.”",
+},
+{
+    author:"Maya Angelou",
+    quoteDescription:"“I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”",
+}
+];
+
+
+var quoteBtn = document.getElementById('getQuote')
+
