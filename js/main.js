6bad338ff0d81c611bcb3836a2b131140e55cea4
RaedElnady <raed.elnady4@gmail.com>
2022-05-14 12:14:29 +0300

    Adding the JS logic

--- js/main.js
+++ js/main.js
@@ -18,9 +18,44 @@ var quotes = [
 {
     author:"Maya Angelou",
     quoteDescription:"“I've learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”",
+},
+{
+    author:"Thomas A. Edison",
+    quoteDescription:"“I have not failed. I've just found 10,000 ways that won't work.”",
 }
 ];
 
 
 var quoteBtn = document.getElementById('getQuote')
 
+var testA = 'AAAAAAAAAA'
+var testB = 'BBBBBBBBBB'
+
+var quoteOutput = document.getElementById('quoteOutput')
+var authorOutput = document.getElementById('authorOutput')
+
+currentIndex = 0
+
+quoteBtn.onclick=function(){
+    
+    for (var i=0;i<quotes.length;i++){
+        if (currentIndex==i){
+            displayData()
+
+        }
+        else if(currentIndex==quotes.length){
+            currentIndex=0
+            displayData()
+        }
+    }
+    currentIndex +=1
+    console.log(currentIndex)
+    console.log(quoteOutput)
+    console.log(authorOutput)
+    
+}
+
+var displayData = function(){
+    quoteOutput.innerHTML=quotes[currentIndex].quoteDescription
+    authorOutput.innerHTML=quotes[currentIndex].author
+}
